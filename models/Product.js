339de4a60326854e2deed66f10ddfbf86aa621d5
unknown <anthony_rosati@yahoo.com>
2023-12-25 19:44:38 -0600

    Updated invidual models in models folder

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,44 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // Checks to confirm value provided is a decimal
+      validate: {
+        isDecimal: true
+      }
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      // Checks to confirm value provided is numeric
+      validate: {
+        isNumeric: true
+      }
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
